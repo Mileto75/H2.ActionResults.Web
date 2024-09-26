76ee8fadc7e36ca9157d975767ba008fc6558bbb
Mileto Di Marco <mileto.di.marco@howest.be>
2024-09-26 09:55:58 +0200

    Finish solution

--- MyFirstMvcApp.Web/Controllers/HomeController.cs
+++ MyFirstMvcApp.Web/Controllers/HomeController.cs
@@ -40,6 +40,7 @@ namespace MyFirstMvcApp.Web.Controllers
             return View();
         }
 
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ MyFirstMvcApp.Web/Controllers/ResponseTypesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstMvcApp.Web.Controllers
+{
+    public class ResponseTypesController : Controller
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public ResponseTypesController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public IActionResult Fail()
+        {
+            return NotFound();
+        }
+        public IActionResult Forward() 
+        {
+            //return Redirect("/home/whatsmyname");
+            return RedirectToAction("Whatsmyname","Home");
+        }
+        public IActionResult Download()
+        {
+            //return File("/docs/controllers.pdf", "application/pdf");
+
+            var pathToFile = Path.Combine(_webHostEnvironment.WebRootPath, "docs", "controllers.pdf");
+            var fileStream = new FileStream(pathToFile, FileMode.Open);
+            return File(fileStream, "application/pdf");
+        }
+    }
+}
