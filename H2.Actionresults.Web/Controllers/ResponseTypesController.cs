2adecd10f64bccc9b902365ac27d6a51509e1a4d
Mileto Di Marco <mileto.di.marco@howest.be>
2025-09-25 11:31:45 +0200

    Add part 2

---
+++ H2.Actionresults.Web/Controllers/ResponseTypesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace H2.Actionresults.Web.Controllers
+{
+    public class ResponseTypesController : Controller
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public ResponseTypesController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public IActionResult Fail()
+        {
+            return NotFound();
+        }
+        public IActionResult Forward()
+        {
+            return Redirect("/Home/WhatsMyName");
+            //return RedirectToAction("WhatsMyName","Home");
+        }
+        public IActionResult Download()
+        {
+            var path = Path.Combine(_webHostEnvironment.WebRootPath, "files", "h02-1.controllers.pdf");
+            var filestream = new FileStream(path, FileMode.Open);
+            return File(filestream, "application/pdf","oefening1.pdf");
+        }
+    }
+}
